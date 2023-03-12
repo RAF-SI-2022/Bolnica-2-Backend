8de99bda5f9a8fb46f571ddde71003e50a2c24e7
Milos Ceculovic <mceculovic2819rn@raf.rs>
2023-03-13 00:00:19 +0100

    Added count for listing users and lbz

--- user-service/src/main/java/com/raf/si/userservice/service/UserService.java
+++ user-service/src/main/java/com/raf/si/userservice/service/UserService.java
@@ -3,13 +3,11 @@ package com.raf.si.userservice.service;
 import com.raf.si.userservice.dto.request.CreateUserRequest;
 import com.raf.si.userservice.dto.request.PasswordResetRequest;
 import com.raf.si.userservice.dto.request.UpdateUserRequest;
-import com.raf.si.userservice.dto.response.CountResponse;
 import com.raf.si.userservice.dto.response.MessageResponse;
-import com.raf.si.userservice.dto.response.UserListResponse;
+import com.raf.si.userservice.dto.response.UserListAndCountResponse;
 import com.raf.si.userservice.dto.response.UserResponse;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
@@ -24,10 +22,8 @@ public interface UserService {
 
     UserResponse updateUser(UUID lbz, UpdateUserRequest updateUserRequest);
 
-    List<UserListResponse> listUsers(String firstName, String lastName, String departmentName,
-                                     String hospitalName, boolean includeDeleted, Pageable pageable);
+    UserListAndCountResponse listUsers(String firstName, String lastName, String departmentName,
+                                       String hospitalName, boolean includeDeleted, Pageable pageable);
 
     MessageResponse resetPassword(PasswordResetRequest passwordResetRequest);
-
-    CountResponse getUsersCount();
 }
