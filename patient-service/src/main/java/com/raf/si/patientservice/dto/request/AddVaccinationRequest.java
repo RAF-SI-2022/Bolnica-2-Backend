ff24c0f06f43d4e34a5b37e20c8b69ef944e3d10
novak2000 <nzivanic2319rn@raf.rs>
2023-03-23 13:32:29 +0100

    add requests and responses for healthrecord updates

---
+++ patient-service/src/main/java/com/raf/si/patientservice/dto/request/AddVaccinationRequest.java
@@ -0,0 +1,22 @@
+package com.raf.si.patientservice.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AddVaccinationRequest {
+
+    @NotEmpty(message = "Polje ime vakcine ne sme da bude prazno")
+    private String vaccine;
+
+    @NotEmpty(message = "Polje datum vakcinacije ne sme da bude prazno")
+    private Date allergen;
+
+
+}
