9b1b15f8df8c955034b1cc9f9f0ced45058d4678
Filip Filipovic <filip@Filips-MacBook-Air.local>
2023-03-27 00:15:16 +0200

    Poboljsan bootstrap

--- patient-service/src/main/java/com/raf/si/patientservice/bootstrap/BootstrapData.java
+++ patient-service/src/main/java/com/raf/si/patientservice/bootstrap/BootstrapData.java
@@ -5,8 +5,7 @@ import com.raf.si.patientservice.model.*;
 import com.raf.si.patientservice.model.enums.healthrecord.BloodType;
 import com.raf.si.patientservice.model.enums.healthrecord.RHFactor;
 import com.raf.si.patientservice.model.enums.medicalhistory.TreatmentResult;
-import com.raf.si.patientservice.model.enums.patient.CountryCode;
-import com.raf.si.patientservice.model.enums.patient.Gender;
+import com.raf.si.patientservice.model.enums.patient.*;
 import com.raf.si.patientservice.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -70,6 +69,18 @@ public class BootstrapData implements CommandLineRunner {
         patient.setCountryOfLiving(CountryCode.AFG);
         patient.setLbp(UUID.fromString("c208f04d-9551-404e-8c54-9321f3ae9be8"));
 
+        patient.setAddress("Jurija Gagarina 16");
+        patient.setPlaceOfLiving("Novi Beograd");
+        patient.setPhoneNumber("0601234567");
+        patient.setEmail("pacijent.pacijentovic@gmail.com");
+        patient.setCustodianJmbg("0101987123456");
+        patient.setCustodianName("Staratelj Starateljovic");
+        patient.setFamilyStatus(FamilyStatus.OBA_RODITELJA);
+        patient.setMaritalStatus(MaritalStatus.SAMAC);
+        patient.setChildrenNum(0);
+        patient.setEducation(Education.VISOKO_OBRAZOVANJE);
+        patient.setProfession("Programer");
+
 
         HealthRecord healthRecord = new HealthRecord();
         healthRecord.setRegistrationDate(new Date());
@@ -85,27 +96,34 @@ public class BootstrapData implements CommandLineRunner {
         operation.setHealthRecord(healthRecord);
 
 
+        Diagnosis diagnosis = new Diagnosis();
+        diagnosis.setCode("A15.3");
+        diagnosis.setDescription("Grip, virus nedokazan");
+        diagnosis.setLatinDescription("Influenza, virus non identificatum");
+        diagnosisRepository.save(diagnosis);
+
         MedicalExamination examination = new MedicalExamination();
         examination.setLbz(UUID.randomUUID());
         examination.setDate(new Date());
-        examination.setObjectiveFinding("Nadjeno bla bla");
+        examination.setObjectiveFinding("Grip, ne zna se koji virus je uzrok");
+        examination.setMainSymptoms("Oteceno grlo, temperatura");
+        examination.setCurrentIllness("Prehlada");
+        examination.setAnamnesis("Bol u grlu, temperatura, pacijentu je konstantno hladno");
+        examination.setFamilyAnamnesis("Bol u grlu, temperatura");
+        examination.setPatientOpinion("Streptokoke");
+        examination.setDiagnosis(diagnosis);
+        examination.setSuggestedTherapy("Odmor, septolete jednom dnevno");
+        examination.setAdvice("Odmor");
 
         examination.setHealthRecord(healthRecord);
 
         MedicalExamination examination2 = new MedicalExamination();
         examination2.setLbz(UUID.randomUUID());
         examination2.setDate(new Date());
-        examination2.setObjectiveFinding("Nadjeno bla bla 2");
+        examination2.setObjectiveFinding("Male boginje po celom telu");
         examination2.setConfidential(true);
 
         examination2.setHealthRecord(healthRecord);
-
-
-        Diagnosis diagnosis = new Diagnosis();
-        diagnosis.setCode("AAA");
-        diagnosis.setDescription("Opis");
-        diagnosis.setLatinDescription("Latinski opis");
-        diagnosisRepository.save(diagnosis);
 
         MedicalHistory medicalHistory = new MedicalHistory();
         medicalHistory.setDiagnosis(diagnosis);
@@ -130,7 +148,7 @@ public class BootstrapData implements CommandLineRunner {
 
 
         Allergen allergen = new Allergen();
-        allergen.setName("alergija");
+        allergen.setName("polen");
 
         allergenRepository.save(allergen);
 
@@ -141,7 +159,7 @@ public class BootstrapData implements CommandLineRunner {
         allergyRepository.save(allergy);
 
         Allergen allergen2 = new Allergen();
-        allergen2.setName("alergija 2");
+        allergen2.setName("lipa");
 
         allergenRepository.save(allergen2);
 
