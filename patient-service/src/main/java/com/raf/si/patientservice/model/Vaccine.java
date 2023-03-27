54f0a94997b0203eb8b9206caa4cca29341dc6fe
novak2000 <nzivanic2319rn@raf.rs>
2023-03-27 22:09:07 +0200

    set name argument to unique(Allegen,vaccine) add notation to bloodtype enum

--- patient-service/src/main/java/com/raf/si/patientservice/model/Vaccine.java
+++ patient-service/src/main/java/com/raf/si/patientservice/model/Vaccine.java
@@ -16,7 +16,7 @@ public class Vaccine {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
--- patient-service/src/main/java/com/raf/si/patientservice/model/enums/healthrecord/BloodType.java
+++ patient-service/src/main/java/com/raf/si/patientservice/model/enums/healthrecord/BloodType.java
@@ -1,5 +1,28 @@
 package com.raf.si.patientservice.model.enums.healthrecord;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum BloodType {
-    A, B, AB, O
+
+    A("A"),
+    B("B"),
+    AB("AB"),
+    O("0");
+
+    private String notation;
+
+    BloodType(String notation) {
+        this.notation = notation;
+    }
+
+    public static BloodType valueOfNotation(String notation) {
+        for (BloodType g : values()) {
+            if (g.notation.equals(notation)) {
+                return g;
+            }
+        }
+        return null;
+    }
+
 }
