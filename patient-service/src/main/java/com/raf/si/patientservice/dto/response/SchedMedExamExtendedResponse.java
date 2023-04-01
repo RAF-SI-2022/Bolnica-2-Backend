52d2e2459c9e8a3dc350853c6fe8cf522fe78b8e
AlekZivkovic <alek.zivkovic723@gmail.com>
2023-04-02 00:07:12 +0200

    vracen dependency

--- patient-service/src/main/java/com/raf/si/patientservice/dto/response/SchedMedExamExtendedResponse.java
+++ patient-service/src/main/java/com/raf/si/patientservice/dto/response/SchedMedExamExtendedResponse.java
@@ -1,9 +1,7 @@
 package com.raf.si.patientservice.dto.response;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 public class SchedMedExamExtendedResponse extends SchedMedExamResponse{
