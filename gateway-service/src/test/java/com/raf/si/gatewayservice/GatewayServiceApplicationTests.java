2591ad449456d194ba9ecaf6dfc567b0b6abf9a1
dragicevicbranislav <118771349+dragicevicbranislav@users.noreply.github.com>
2023-04-03 18:46:59 +0200

    dodati gateway i discovery servisi

---
+++ gateway-service/src/test/java/com/raf/si/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.si.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
