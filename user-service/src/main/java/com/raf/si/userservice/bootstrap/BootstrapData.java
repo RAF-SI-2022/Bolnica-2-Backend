6b3151ed35ce10946ce8d47b8e072eb0ed445888
Milos Ceculovic <mceculovic2819rn@raf.rs>
2023-04-24 18:10:11 +0200

    Prepravljeni testovi i izmenjen bootstrap podataka

--- user-service/src/main/java/com/raf/si/userservice/bootstrap/BootstrapData.java
+++ user-service/src/main/java/com/raf/si/userservice/bootstrap/BootstrapData.java
@@ -14,10 +14,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
@@ -39,128 +43,197 @@ public class BootstrapData implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
-        Hospital hospital = new Hospital();
-        hospital.setAddress("Dimitrija Tucovića 161");
-        hospital.setDateOfEstablishment(new Date());
-        hospital.setActivity("Hirurgija");
-        hospital.setPlace("Beograd");
-        hospital.setFullName("KBC Zvezdara - Klinika za hirurgiju \"Nikola Spasic\"");
-        hospital.setPbb(UUID.randomUUID());
-        hospital.setShortName("KBC Zvezdara");
+    public void run(String... args) throws ParseException, IOException, URISyntaxException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        hospital = hospitalRepository.save(hospital);
-
-        Department chirurgyDepartment = new Department();
-        chirurgyDepartment.setName("Hirurgija");
-        chirurgyDepartment.setHospital(hospital);
-        chirurgyDepartment.setPbo(UUID.randomUUID());
-
-        Department laboratoryDepartment = new Department();
-        laboratoryDepartment.setName("Laboratorija");
-        laboratoryDepartment.setHospital(hospital);
-        laboratoryDepartment.setPbo(UUID.fromString("4e5911c8-ce7a-11ed-afa1-0242ac120002"));
-
-        Department diagnosticDepartment = new Department();
-        diagnosticDepartment.setName("Dijagnostika");
-        diagnosticDepartment.setHospital(hospital);
-        diagnosticDepartment.setPbo(UUID.randomUUID());
-
-        chirurgyDepartment = departmentRepository.save(chirurgyDepartment);
-        departmentRepository.save(laboratoryDepartment);
-        departmentRepository.save(diagnosticDepartment);
-
-        Permission adminPermission = new Permission();
-        adminPermission.setName("ROLE_ADMIN");
-
-        Permission drSpecOdeljenjaPermission = new Permission();
-        drSpecOdeljenjaPermission.setName("ROLE_DR_SPEC_ODELJENJA");
-
-        Permission drSpecPermission = new Permission();
-        drSpecPermission.setName("ROLE_DR_SPEC");
-
-        Permission drSpecPovPermission = new Permission();
-        drSpecPovPermission.setName("ROLE_DR_SPEC_POV");
-
-        Permission visaMedSestraPermission = new Permission();
-        visaMedSestraPermission.setName("ROLE_VISA_MED_SESTRA");
-
-        Permission medSestraPermission = new Permission();
-        medSestraPermission.setName("ROLE_MED_SESTRA");
-
-        Permission visiLabTehnicar = new Permission();
-        visiLabTehnicar.setName("ROLE_VISI_LAB_TEHNICAR");
-
-        Permission labTehnicar = new Permission();
-        labTehnicar.setName("ROLE_LAB_TEHNICAR");
-
-        Permission medBiohemicar = new Permission();
-        medBiohemicar.setName("ROLE_MED_BIOHEMICAR");
-
-        Permission specMedBiohemije = new Permission();
-        specMedBiohemije.setName("ROLE_SPEC_MED_BIOHEMIJE");
-
-        Permission receptionistPermission = new Permission();
-        receptionistPermission.setName("ROLE_RECEPCIONER");
-
-        List<Permission> adminPermissions = new ArrayList<>();
-        List<Permission> medSestraPermissions = new ArrayList<>();
-        visaMedSestraPermission = permissionsRepository.save(visaMedSestraPermission);
-        medSestraPermission = permissionsRepository.save(medSestraPermission);
+        Hospital kbcZvezdara = addHospital("Dimitrija Tucovića 161", "01/12/1935", "Zdravstvena delatnost",
+                "Beograd", "Kliničko bolnički centar \"Zvezdara\"", "KBC Zvezdara");
 
 
-        adminPermissions.add(permissionsRepository.save(adminPermission));
-        adminPermissions.add(permissionsRepository.save(drSpecOdeljenjaPermission));
-        adminPermissions.add(permissionsRepository.save(drSpecPermission));
-        adminPermissions.add(permissionsRepository.save(drSpecPovPermission));
-        adminPermissions.add(permissionsRepository.save(visiLabTehnicar));
-        adminPermissions.add(permissionsRepository.save(labTehnicar));
-        adminPermissions.add(permissionsRepository.save(medBiohemicar));
-        adminPermissions.add(permissionsRepository.save(specMedBiohemije));
-        adminPermissions.add(permissionsRepository.save(receptionistPermission));
-        adminPermissions.add(visaMedSestraPermission);
-        adminPermissions.add(medSestraPermission);
+        Hospital ukcSrbija = addHospital("Pasterova 2", "01/01/1983", "Zdravstvena delatnost",
+                "Beograd", "Univerzitetski klinički centar Srbije", "UKC Srbije");
 
-        medSestraPermissions.add(visaMedSestraPermission);
-        medSestraPermissions.add(medSestraPermission);
+
+        Hospital kbcBezanijskaKosa = addHospital("Bezanijska kosa bb", "01/01/2000", "Zdravstvena delatnost",
+                "Beograd", "Kliničko bolnički centar \"Bežanijska kosa\"", "KBC Bežanijska kosa");
+
+
+        Hospital kbcZemun = addHospital("Vukova 9", "25/02/1784", "Zdravstvena delatnost",
+                "Beograd", "Kliničko bolnički centar \"Zemun\"", "KBC Zemun");
+
+
+        addDepartmentsToHospital(
+                List.of("Klinika za ginekologiju", "Klinika za hirurgiju", "Klinika za interne bolesti",
+                        "Klinika za pedijatriju", "Klinika za psihijatriju", "Klinika za očne bolesti",
+                        "Klinika za uho grlo i nos", "Služba za dijagnostičke procedure"),
+                kbcZvezdara
+        );
+
+        addDepartmentsToHospital(
+                List.of("Klinika za infektivne i tropske bolesti", "Klinika za interne bolesti",
+                        "Klinika za psihijatriju", "Klinika za neurologiju", "Klinika za hirurgiju",
+                        "Klinika za ginekologiju i akušerstvo", "Klinika za očne bolesti",
+                        "Klinika za uho, grlo i nos", "Klinika za dermatovenerologiju", "Klinika za onkologiju",
+                        "Služba za dijagnostičke procedure"),
+                ukcSrbija
+        );
+
+        addDepartmentsToHospital(
+                List.of("Klinika za interne bolesti", "Klinika za hirurgiju",
+                        "Klinika za onkologiju", "Služba za dijagnostičke procedure"),
+                kbcBezanijskaKosa
+        );
+
+        addDepartmentsToHospital(
+                List.of("Klinika za interne bolesti", "Klinika za neurologiju",
+                        "Klinika za hirurgiju", "Klinika za ginekologiju i akušerstvo",
+                        "Klinika za onkologiju", "Služba za dijagnostičke procedure"),
+                kbcZemun
+        );
+
+        Permission adminPermission = addPermission("ROLE_ADMIN");
+
+        Permission drSpecOdeljenjaPermission = addPermission("ROLE_DR_SPEC_ODELJENJA");
+
+        Permission drSpecPermission = addPermission("ROLE_DR_SPEC");
+
+        Permission drSpecPovPermission = addPermission("ROLE_DR_SPEC_POV");
+
+        Permission visaMedSestraPermission = addPermission("ROLE_VISA_MED_SESTRA");
+
+        Permission medSestraPermission = addPermission("ROLE_MED_SESTRA");
+
+        Permission visiLabTehnicar = addPermission("ROLE_VISI_LAB_TEHNICAR");
+
+        Permission labTehnicar = addPermission("ROLE_LAB_TEHNICAR");
+
+        Permission medBiohemicar = addPermission("ROLE_MED_BIOHEMICAR");
+
+        Permission specMedBiohemije = addPermission("ROLE_SPEC_MED_BIOHEMIJE");
+
+        Permission receptionistPermission = addPermission("ROLE_RECEPCIONER");
+
+        List<Permission> adminPermissions = new ArrayList<>(
+                List.of(adminPermission, drSpecOdeljenjaPermission, drSpecPermission, drSpecPovPermission, visiLabTehnicar,
+                        labTehnicar, medBiohemicar, specMedBiohemije, receptionistPermission, visaMedSestraPermission,
+                        medSestraPermission)
+        );
+
+        List<Permission> medSestraPermissions = new ArrayList<>(List.of(medSestraPermission, visaMedSestraPermission));
 
         User user = new User();
         user.setEmail("balkan.medic2023@outlook.com");
         user.setPassword(passwordEncoder.encode("admin"));
         user.setUsername("admin");
-        user.setDepartment(chirurgyDepartment);
-        user.setResidentialAddress("Admin address");
+        user.setDepartment(departmentRepository.
+                findDepartmentByNameAndHospital_ShortName("Klinika za hirurgiju", ukcSrbija.getShortName()));
+        user.setResidentialAddress("Ljubiše Jelenkovića 73");
         user.setPermissions(adminPermissions);
-        user.setJMBG("23112412212");
+        user.setJMBG("2013012312143");
         user.setPhone("02132123132");
-        user.setPlaceOfLiving("Place of living");
-        user.setDateOfBirth(new Date());
+        user.setPlaceOfLiving("Beograd, Srbija");
+        user.setDateOfBirth(sdf.parse("02/05/1965"));
         user.setGender("Muski");
-        user.setFirstName("Admin");
-        user.setLastName("Adminovic");
-        user.setTitle(Title.DR_SCI_MED);
+        user.setFirstName("Zlatibor");
+        user.setLastName("Lončar");
+        user.setTitle(Title.PROF_DR_MED);
         user.setProfession(Profession.SPEC_HIRURG);
         user.setLbz(UUID.fromString("5a2e71bb-e4ee-43dd-a3ad-28e043f8b435"));
 
         User medSestra = new User();
-        medSestra.setEmail("medsestra@something.com");
+        medSestra.setEmail("edotlic10320rn@raf.rs");
         medSestra.setPassword(passwordEncoder.encode("medsestra"));
         medSestra.setUsername("medsestra");
-        medSestra.setDepartment(laboratoryDepartment);
+        medSestra.setDepartment(departmentRepository.
+                findDepartmentByNameAndHospital_ShortName("Služba za dijagnostičke procedure", ukcSrbija.getShortName()));
         medSestra.setResidentialAddress("address");
         medSestra.setPermissions(medSestraPermissions);
-        medSestra.setJMBG("463453453434");
-        medSestra.setPhone("0213123112");
-        medSestra.setPlaceOfLiving("Place");
-        medSestra.setDateOfBirth(new Date());
+        medSestra.setJMBG("2002010359910");
+        medSestra.setPhone("0621231231");
+        medSestra.setPlaceOfLiving("Cara Dušana 141");
+        medSestra.setDateOfBirth(sdf.parse("18/02/1995"));
         medSestra.setGender("Zenski");
-        medSestra.setFirstName("Medicinska");
-        medSestra.setLastName("Sestra");
+        medSestra.setFirstName("Emilija");
+        medSestra.setLastName("Dotlić");
         medSestra.setTitle(Title.DIPL_FARM);
         medSestra.setProfession(Profession.MED_SESTRA);
         medSestra.setLbz(UUID.fromString("3e1a51ab-a3aa-1add-a3ad-28e043f8b435"));
 
-        userRepository.save(medSestra);
         userRepository.save(user);
+        userRepository.save(medSestra);
+
+        addOtherUsers();
+    }
+
+    private Hospital addHospital(String address, String date, String activity,
+                                 String place, String fullName, String shortName) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Hospital hospital = new Hospital();
+
+        hospital.setAddress(address);
+        hospital.setDateOfEstablishment(sdf.parse(date));
+        hospital.setActivity(activity);
+        hospital.setPlace(place);
+        hospital.setFullName(fullName);
+        hospital.setShortName(shortName);
+        hospital.setPbb(UUID.randomUUID());
+
+        return hospitalRepository.save(hospital);
+    }
+
+    private void addDepartmentsToHospital(List<String> departmentNames, Hospital hospital) {
+        for (String s : departmentNames) {
+            Department department = new Department();
+            department.setName(s);
+            department.setHospital(hospital);
+            department.setPbo(UUID.randomUUID());
+            departmentRepository.save(department);
+        }
+    }
+
+    private Permission addPermission(String role) {
+        Permission permission = new Permission();
+        permission.setName(role);
+        permission = permissionsRepository.save(permission);
+
+        return permission;
+    }
+
+    private void addOtherUsers() throws URISyntaxException, IOException, ParseException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("bootstrap-data/employees.txt");
+        if (resource == null) {
+            throw new IllegalArgumentException("Cannot load bootstrap file!");
+        }
+
+        List<String> lines = Files.readAllLines(Path.of(resource.toURI()));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        for(String s: lines) {
+            if(s.isEmpty())
+                continue;
+            String[] split = s.split(",");
+            String usernamePassword = split[0].substring(0, split[0].indexOf('@'));
+            User user = new User();
+            user.setEmail(split[0]);
+            user.setPassword(passwordEncoder.encode(usernamePassword));
+            user.setUsername(passwordEncoder.encode(usernamePassword));
+            user.setDepartment(departmentRepository.
+                    findDepartmentByNameAndHospital_ShortName(split[1], split[2]));
+            user.setResidentialAddress(split[3]);
+            user.setPermissions(permissionsRepository.findPermissionsByNameIsIn(Arrays.asList(split[4].split(";"))));
+            user.setJMBG(split[5]);
+            user.setPhone(split[6]);
+            user.setPlaceOfLiving(split[7].replace(";", ","));
+            user.setDateOfBirth(sdf.parse(split[8]));
+            user.setGender(split[9]);
+            user.setFirstName(split[10]);
+            user.setLastName(split[11]);
+            user.setTitle(Title.valueOfNotation(split[12]));
+            user.setProfession(Profession.valueOfNotation(split[13]));
+            user.setLbz(UUID.randomUUID());
+            userRepository.save(user);
+        }
+
     }
 }
