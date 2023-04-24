0a36315a2ff648f82803888c531d90136c0279c2
Filip Filipovic <filip@Filips-MacBook-Air.local>
2023-04-24 22:16:09 +0200

    Fix za Role koje mogu da pristupe nekim metodama

--- laboratory-service/src/main/java/com/raf/si/laboratoryservice/controller/WorkOrderController.java
+++ laboratory-service/src/main/java/com/raf/si/laboratoryservice/controller/WorkOrderController.java
@@ -37,7 +37,7 @@ public class WorkOrderController {
         return ResponseEntity.ok(orderService.orderHistory(historyRequest, PageRequest.of(page, size)));
     }
 
-    @PreAuthorize("hasRole('ROLE_MED_BIOHEM') or hasRole('ROLE_SPEC_MED_BIOHEM')")
+    @PreAuthorize("hasRole('ROLE_MED_BIOHEMICAR') or hasRole('ROLE_SPEC_MED_BIOHEMIJE')")
     @PutMapping("/saveResult")
     public ResponseEntity<SaveResultResponse> saveResult(@Valid @RequestBody SaveResultRequest saveRequest){
         return ResponseEntity.ok(orderService.saveResult(saveRequest));
@@ -45,15 +45,15 @@ public class WorkOrderController {
 
     @PreAuthorize("hasRole('ROLE_DR_SPEC_ODELJENJA') or hasRole('ROLE_DR_SPEC') " +
             "or hasRole('ROLE_DR_SPEC_POV') or hasRole('ROLE_LAB_TEHNICAR')" +
-            " or hasRole('ROLE_VISI_LAB_TEHNICAR') or hasRole('ROLE_MED_BIOHEM')" +
-            "or hasRole('ROLE_SPEC_MED_BIOHEM')")
+            " or hasRole('ROLE_VISI_LAB_TEHNICAR') or hasRole('ROLE_MED_BIOHEMICAR')" +
+            "or hasRole('ROLE_SPEC_MED_BIOHEMIJE')")
     @GetMapping("/results/{orderId}")
     public ResponseEntity<ResultResponse> getResults(@PathVariable("orderId") Long orderId){
         return ResponseEntity.ok(orderService.getResults(orderId));
     }
 
     @PreAuthorize("hasRole('ROLE_LAB_TEHNICAR') or hasRole('ROLE_VISI_LAB_TEHNICAR')" +
-            " or hasRole('ROLE_MED_BIOHEM') or hasRole('ROLE_SPEC_MED_BIOHEM')")
+            " or hasRole('ROLE_MED_BIOHEMICAR') or hasRole('ROLE_SPEC_MED_BIOHEMIJE')")
     @PostMapping("/historyForLab")
     public ResponseEntity<OrderHistoryResponse> orderHistoryForLab(@Valid @RequestBody OrderHistoryForLabRequest historyRequest,
                                                                    @RequestParam(defaultValue = "0") int page,
@@ -61,7 +61,7 @@ public class WorkOrderController {
         return ResponseEntity.ok(orderService.orderHistoryForLab(historyRequest, PageRequest.of(page, size)));
     }
 
-    @PreAuthorize("hasRole('ROLE_SPEC_MED_BIOHEM')")
+    @PreAuthorize("hasRole('ROLE_SPEC_MED_BIOHEMIJE')")
     @PostMapping("/verify/{orderId}")
     public ResponseEntity<OrderResponse> verify(@PathVariable("orderId") Long orderId){
         return ResponseEntity.ok(orderService.verify(orderId));
