828d42dd08a453b159e3911a7302b85d6fbfeaa9
Filip Filipovic <filip@Filips-MacBook-Air.local>
2023-05-15 22:48:52 +0200

    Fix za appointment repository

--- patient-service/src/main/java/com/raf/si/patientservice/repository/AppointmentRepository.java
+++ patient-service/src/main/java/com/raf/si/patientservice/repository/AppointmentRepository.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>, JpaSpecificationExecutor<Appointment> {
 
-    @Query(value = "select case (count(a) > 0) then true else false end " +
+    @Query(value = "select case when (count(a) > 0) then true else false end " +
             "from Appointment a where a.patient=:patient and a.receiptDate between :startDate and :endDate")
     boolean patientHasAppointmentDateBetween(Patient patient, Date startDate, Date endDate);
 }
